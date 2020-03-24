5a9f154d745a40f3561d01654edb81428245ba37
Curso <Curso@LSTKMY231256.stktrainning.com>
2020-03-24 16:42:00 -0600

    admin/ListProducts

--- EcommerseAdmin/EcommerseAdmin/Controllers/HomeController.cs
+++ EcommerseAdmin/EcommerseAdmin/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using EcommerseAdmin.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace EcommerseAdmin.Controllers
 {
@@ -17,9 +19,15 @@ namespace EcommerseAdmin.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            string pathController = "api/ProductCatalogService?pageNumber=1";
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5001/");
+            var response = client.GetAsync(pathController);
+            response.Wait();
+            var result = response.Result;
+            var readresult = result.Content.ReadAsStringAsync().Result;
+            var resultadoFinal = JsonConvert.DeserializeObject<ProductCatalog>(readresult);
+            return View(resultadoFinal);
         }
 
         public IActionResult Contact()
