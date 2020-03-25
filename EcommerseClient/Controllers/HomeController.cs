5867ba5310f91a5007954b23a7b3e7116173ad65
Curso <Curso@LSTKMY231256.stktrainning.com>
2020-03-25 14:28:40 -0600

    client cambios

--- EcommerseClient/Controllers/HomeController.cs
+++ EcommerseClient/Controllers/HomeController.cs
@@ -5,19 +5,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using EcommerseClient.Models;
-<<<<<<< HEAD
 using System.Net.Http;
 using Newtonsoft.Json;
-=======
->>>>>>> 4079f7fb95f1147cd294c810cc937e91c2ae9614
+using System.Text;
 
 namespace EcommerseClient.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string currency)
         {
-<<<<<<< HEAD
             string pathController = "api/ProductCatalogService?pageNumber=1";
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("http://localhost:5001/");
@@ -26,11 +23,47 @@ namespace EcommerseClient.Controllers
             var result = response.Result;
             var readresult = result.Content.ReadAsStringAsync().Result;
             var resultadoFinal = JsonConvert.DeserializeObject<ProductCatalog>(readresult);
+
+            if (currency != null && currency != "USD")
+            {
+                foreach (var item in resultadoFinal.products)
+                {
+                    HttpClient clientt = new HttpClient();
+                    string path = "api/currency/conversion";
+                    clientt.BaseAddress = new Uri("http://localhost:5004/");
+                    string json = JsonConvert.SerializeObject(
+                        new CurrencyChange() {CurrencyCode = "USD", CurrencyType = currency, Nano = item.priceUsd.nanos,
+                        Units = item.priceUsd.units});
+                    var httpcontent = new StringContent(json, Encoding.UTF8, "application/json");
+                    var resp = clientt.PostAsync(path, httpcontent);
+                    resp.Wait();
+                    var resul = resp.Result;
+                    var readresul = resul.Content.ReadAsStringAsync().Result;
+                    string resulFin = JsonConvert.DeserializeObject<double>(readresul).ToString();
+                    string[] separators = { "." };
+                    string[] words = resulFin.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                    try
+                    {
+                        item.priceUsd.currencyCode = currency;
+                        item.priceUsd.units = int.Parse(words[0]);
+                        item.priceUsd.nanos = Int32.Parse(words[1]);
+                    }
+                    catch (Exception)
+                    {
+                        item.priceUsd.units = 69;
+                        item.priceUsd.nanos = 69;
+                    }
+                }
+            }
+
             return View(resultadoFinal);
-=======
-            return View();
->>>>>>> 4079f7fb95f1147cd294c810cc937e91c2ae9614
         }
+
+        public IActionResult BuyProduct()
+        {
+            return View();
+        }
+
 
         public IActionResult About()
         {
